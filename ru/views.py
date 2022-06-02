8bbc7eebb78147242e63b5b554ab933a7b635fe6
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2022-06-01 21:29:55 -0300

    Criando o Cronjob

--- ru/views.py
+++ ru/views.py
@@ -7,10 +7,6 @@ from .models import RU
 from .serializers import RUSerializer
 from datetime import datetime
 
-def updateInfo():
-    # RU.objects.all().delete()
-    pass
-
 
 # Create your views here.
 class RUView(APIView):
@@ -18,11 +14,5 @@ class RUView(APIView):
     def get(self, request):
 
         ru = RU.objects.all()
-        if len(ru) == 0 or ru[len(ru)-1].created_at.date() != datetime.date.today():
-            updateInfo()
-            ru = RU.objects.all()
-
         serializer = RUSerializer(ru, many=True)
         return Response(serializer.data)
-
-

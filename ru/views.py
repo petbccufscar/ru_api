da2b31e6e42eebadf09fa6e6ac5ffc79f26d7523
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2022-06-06 13:51:22 -0300

    Adiciona cache de respostas

--- ru/views.py
+++ ru/views.py
@@ -1,5 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.shortcuts import render
-# Create api view 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,11 +9,9 @@ from .serializers import RUSerializer
 from datetime import datetime
 
 
-# Create your views here.
 class RUView(APIView):
-
+    @method_decorator(cache_page(60))
     def get(self, request):
-
         ru = RU.objects.all()
         serializer = RUSerializer(ru, many=True)
         return Response(serializer.data)

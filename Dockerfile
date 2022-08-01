84dc89613c17dfee0e52c662ee68e29c4215a1a3
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2022-07-31 23:54:48 -0300

    Adapta para o novo site do cardápio
    Co-authored-by: Flávio Filho <flavio.vezono@ufu.br>

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,8 @@
 FROM ubuntu:20.04
 
 RUN apt-get update \
-    && apt-get install -y wget gnupg \
-    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    && apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-    tesseract-ocr tesseract-ocr-por python3-pip google-chrome-stable \
-    fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst \
-    fonts-freefont-ttf libxss1 \
-    && rm -rf /var/lib/apt/lists/*
+    tesseract-ocr tesseract-ocr-por python3-pip
 
 COPY requirements.txt /tmp/requirements.txt
 RUN pip3 install -r /tmp/requirements.txt
--- ru/management/commands/update.py
+++ ru/management/commands/update.py
@@ -1,49 +1,51 @@
+from urllib import request
 from django.core.management.base import BaseCommand
-from html.parser import HTMLParser
 from django.db.utils import IntegrityError
-from pyppeteer import launch
-from PIL import Image, ImageMath
+from PIL import Image, ImageMath, UnidentifiedImageError
 from requests import get
 from pkg_resources import resource_stream
+from bs4 import BeautifulSoup
 from pytesseract import image_to_string
 
 from ru.models import RU
 from datetime import date, datetime, timedelta, timezone
 
-import asyncio
 import re
 import io
 
 MASK = Image.open(resource_stream('ru', 'mask.png')).convert(mode='L')
 
-DATE_PATTERN = '(\d+)\s*/\s*(\d+)'
+DATE_PATTERN = re.compile('(\d+)\s*/\s*(\d+)')
 
 MEALS = [RU.ALMOÇO, RU.JANTAR]
-MEAL_PATTERN = f'({"|".join(MEALS)})'
+MEAL_PATTERN = re.compile(f'({"|".join(MEALS)})')
 
 CAMPUSES = [RU.SÃO_CARLOS, RU.ARARAS, RU.SOROCABA, RU.LAGOA_DO_SINO]
 
-BASE_URL = 'https://www.facebook.com'
-
-# URL for the RU's timeline album.
-ALBUM_URL = BASE_URL + '/media/set/?set=a.160605075654161'
+MENU_URL = (
+    'https://www.proad.ufscar.br/pt-br/servicos/restaurante-universitario'
+)
 
 # Size of the images we expect to be posted.
-IMAGE_SIZE = (960, 540)
+MAIN_IMAGE_SIZE = (1920, 1080)
+
+# Rectangle bounds for the two sub-images to crop.
+# Bounds are written as (left, upper, right, lower).
+SUBL_BOUNDS = (  64, 132,  914, 1054)
+SUBR_BOUNDS = (1004, 132, 1854, 1054)
 
 # Rectangle bounds for where we expect the date to be written.
-# Bounds are written as (left, upper, right, lower).
-DATE_BOUNDS = (254, 72, 690, 108)
+DATE_BOUNDS = (15, 15, 831, 108)
 
 # Rectangle bounds for where we expect data to be written.
-MAIN_DISH_BOUNDS     = (520, 126, 910, 162)
-VEGETARIAN_BOUNDS    = (520, 177, 910, 214)
-EXTRA_BOUNDS         = (520, 229, 910, 265)
-GARNISH_BOUNDS       = (520, 280, 910, 316)
-SALAD_BOUNDS         = (520, 331, 910, 367)
-ACCOMPANIMENT_BOUNDS = (520, 383, 910, 419)
-DESSERT_BOUNDS       = (520, 434, 910, 470)
-JUICE_BOUNDS         = (520, 485, 910, 521)
+MAIN_DISH_BOUNDS     = (429, 106, 826, 201)
+VEGETARIAN_BOUNDS    = (429, 201, 826, 301)
+EXTRA_BOUNDS         = (429, 301, 826, 407)
+GARNISH_BOUNDS       = (429, 407, 826, 509)
+SALAD_BOUNDS         = (429, 509, 826, 614)
+ACCOMPANIMENT_BOUNDS = (429, 614, 826, 715)
+DESSERT_BOUNDS       = (429, 715, 826, 821)
+JUICE_BOUNDS         = (429, 821, 826, 918)
 
 # Dictionary for correcting Tesseract jank on a case-by-case basis.
 CORRECTIONS = {
@@ -54,6 +56,7 @@ CORRECTIONS = {
     'Do': 'do',
     'Da': 'da',
     'Em': 'em',
+    'Fejão': 'Feijão',
     'No': 'no',
     'Na': 'na',
     'Ou': 'ou',
@@ -63,18 +66,6 @@ CORRECTIONS = {
     'Á': 'á',
     'E': 'e',
 }
-
-
-class AlbumHTMLParser(HTMLParser):
-    def __init__(self):
-        self.urls = set()
-        HTMLParser.__init__(self)
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'img':
-            for attr in attrs:
-                if attr[0] == 'src':
-                    self.urls.add(attr[1])
 
 
 class UnrecognizedImageError(Exception):
@@ -102,8 +93,6 @@ def read_img(img, bounds):
 
 class Meal:
     def __init__(self, img):
-        img = preprocess(Image.open(io.BytesIO(img)).resize(IMAGE_SIZE))
-
         # Recognize the date.
         datestr = read_img(img, DATE_BOUNDS)
         date_match = re.search(DATE_PATTERN, datestr)
@@ -140,85 +129,29 @@ class Meal:
         print("\tSuco:", self.juice)
 
 
-async def download_image(url):
-    r = get(url)
-    lm = r.headers.get('last-modified')
-    last_modified = datetime.strptime(lm, '%a, %d %b %Y %H:%M:%S %Z')
-    last_modified = last_modified.replace(tzinfo=timezone.utc)
-    return (r.content, last_modified)
-
-
-async def browse(page, until):
-    await page.goto(ALBUM_URL, waitUntil='networkidle0')
-
-    remaining_scroll_attempts = 8
-    remaining_image_attempts = 8
-    remaining_text_attempts = 8
-    viewed_urls = set()
-    output = []
-
-    while True:
-        # Parse the URLs currently visible.
-        parser = AlbumHTMLParser()
-        parser.feed(await page.content())
-
-        # No new URLs have appeared.
-        # Wait and try some more times.
-        if parser.urls.issubset(viewed_urls):
-            remaining_scroll_attempts -= 1
-            print(f"SCROLL: {remaining_scroll_attempts} attempts remaining.")
-            await page.waitFor(10_000)
-            if remaining_scroll_attempts == 0:
-                return output
-        else:
-            remaining_scroll_attempts = 8
-
-        # Scroll to the bottom.
-        imgs = await page.JJ('img')
-        await imgs[-1].hover()
-
-        for url in parser.urls - viewed_urls:
-            await page.waitFor(10_000)
-            img, last_modified = await download_image(url)
-
-            # Reached a post that is older than requested.
-            # Try skipping it, give up if more than 8 appear in a row.
-            if last_modified < until:
-                remaining_image_attempts -= 1
-                print(f"IMG: {remaining_image_attempts} attempts remaining.")
-                if remaining_image_attempts == 0:
-                    return output
-            else:
-                remaining_image_attempts = 8
-
-            try:
-                meal = Meal(img)
-                meal.display()
-                output.append(meal)
-                remaining_text_attempts = 8
-            except UnrecognizedImageError as _:
-                remaining_text_attempts -= 1
-                print(f"TEXT: {remaining_text_attempts} attempts remaining.")
-                if remaining_text_attempts == 0:
-                    return output
-
-        # Add parsed URLs to the viewed list.
-        viewed_urls = viewed_urls.union(parser.urls)
-
-
-async def get_meals(until):
-    browser = await launch(
-        executablePath='google-chrome-stable',
-        args=['--no-sandbox'],
-        handleSIGINT=False,
-        handleSIGTERM=False,
-        handleSIGHUP=False,
-    )
-
-    try:
-        return await browse(await browser.newPage(), until)
-    finally:
-        await browser.close()
+def get_meals():
+    out = []
+    soup = BeautifulSoup(get(MENU_URL).text, 'html.parser')
+    for img in soup.find_all('img'):
+        print(img['src'].split('/@@')[0])
+        content = get(img['src'].split('/@@')[0]).content
+        try:
+            image = Image.open(io.BytesIO(content)).resize(MAIN_IMAGE_SIZE)
+        except UnidentifiedImageError:
+            continue
+        try:
+            meal = Meal(preprocess(image.crop(SUBL_BOUNDS)))
+            meal.display()
+            out.append(meal)
+        except UnrecognizedImageError:
+            print("fail")
+        try:
+            meal = Meal(preprocess(image.crop(SUBR_BOUNDS)))
+            meal.display()
+            out.append(meal)
+        except UnrecognizedImageError:
+            print("fail")
+    return out
 
 
 def nearest_date(month, day):
@@ -257,23 +190,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Begin update")
 
-        # Prune objects older than a week, but keep at least 1.
-        count = RU.objects.count()
+        # Prune objects older than a week.
         for obj in RU.objects.all():
             age = date.today() - obj.date
-            if age.days > 7 and count > 1:
+            if age.days > 7:
                 obj.delete()
-                count = count - 1
 
-        # Look for posts younger than the last time we checked. Or one week old
-        # if the DB is empty.
-        until = None
-        if count > 0:
-            until = RU.objects.latest('created_at').created_at
-        else:
-            until = datetime.now(tz=timezone.utc) - timedelta(weeks=1)
-
-        meals = asyncio.new_event_loop().run_until_complete(get_meals(until))
-        store_meals(meals)
+        store_meals(get_meals())
 
         print("Update success")

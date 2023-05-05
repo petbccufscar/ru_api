e49f4adb07d31f1ac260275c796c1d6ef5ad16fd
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2023-05-05 14:48:51 -0300

    Arruma erro de data especial do Expo

--- planner_updates/serializers.py
+++ planner_updates/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Update, Asset
+from datetime import timezone
 
 
 class AssetSerializer(serializers.ModelSerializer):
@@ -28,6 +29,11 @@ class LaunchAssetSerializer(serializers.ModelSerializer):
 class UpdateSerializer(serializers.ModelSerializer):
     launch_asset = LaunchAssetSerializer(many=False, read_only=True)
     assets = AssetSerializer(many=True, read_only=True)
+
+    created_at = serializers.DateTimeField(
+        format='%Y-%m-%dT%H:%M:%S.000Z',
+        default_timezone=timezone.utc,
+    )
 
     class Meta:
         model = Update
